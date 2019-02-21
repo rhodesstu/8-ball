a2dc2d3dac157847e02bc41c106ac274ee248a90
Stewart Rhodes <stewart.c.rhodes@gmail.com>
2019-02-20 19:06:01 -0500

    finished 8ball shaker

--- 8ball/8ball/ViewController.swift
+++ 8ball/8ball/ViewController.swift
@@ -9,12 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    
+    var randomBallNumber: Int = 0
 
+    @IBOutlet weak var eightBallImageView1: UIImageView!
+   
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        newBallImage()
+       
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        
+        newBallImage()
+    }
+    
+    func newBallImage () {
+        randomBallNumber = Int.random(in: 0 ... 4)
+        
+        eightBallImageView1.image = UIImage(named: ballArray[randomBallNumber])
+        
+    }
 }
 
